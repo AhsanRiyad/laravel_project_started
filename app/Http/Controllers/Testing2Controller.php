e80578eec45a67dcfe83c445a96c55b91ce8e42a
unknown <riyad298@gmail.com>
2019-04-15 22:01:26 +0600

    commit by alias
    this is used for short cut puspose

---
+++ app/Http/Controllers/Testing2Controller.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\testing2;
+use Illuminate\Http\Request;
+
+class Testing2Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\testing2  $testing2
+     * @return \Illuminate\Http\Response
+     */
+    public function show(testing2 $testing2)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\testing2  $testing2
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(testing2 $testing2)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\testing2  $testing2
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, testing2 $testing2)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\testing2  $testing2
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(testing2 $testing2)
+    {
+        //
+    }
+}
