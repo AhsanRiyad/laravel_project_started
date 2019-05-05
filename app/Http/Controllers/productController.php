3ea4f31b1c7d1678e6a89cfca9ba64dc3ff1ccaa
AhsanRiyad <riyad298@gmail.com>
2019-05-06 02:39:03 +0600

    a working example

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -467,7 +467,7 @@ public function confirmOrderPost(Request $req){
   ///return $receiverName;
 
 
-   dispatch(new sendEmailJob( $uid ,  $payment_method ,  $receiverEmail , $receiverName ))->delay(Carbon::now()->addSeconds(5));
+   dispatch(new sendEmailJob( $uid ,  $payment_method ,  $receiverEmail , $receiverName ))->delay(Carbon::now()->addSeconds(1));
 
 
   $r = [  'cart_count' => $req->s_cart_count , 'loginStatus' => $req->s_login_status ];
