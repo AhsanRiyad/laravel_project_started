a093fcc54631b7b3c59d2b6b91f7330442378b96
AhsanRiyad <riyad298@gmail.com>
2019-06-15 00:04:01 +0600

    migration added

---
+++ database/migrations/2019_06_14_174321_create_meal_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMealTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meal', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->bigInteger('user_id');
+            $table->bigInteger('added_by');
+            $table->double('amount', 8, 2);
+            $table->date('date');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meal');
+    }
+}
